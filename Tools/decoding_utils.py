a7d911ef485d05cf8a9cf0a0504e7b114ac96ef4
Andrei Barcovschi <abarcovschi@gmail.com>
2022-10-07 17:42:12 +0000

    Refactor scripts, extract common functionality to Tools module (called utilities), finalise script for transcribing entire voxceleb1 dataset.

--- Tools/decoding_utils.py
+++ Tools/decoding_utils.py
@@ -1,22 +1,54 @@
 import torch
+import torchaudio
+from typing import List
+from abc import ABC, abstractmethod
 
-# greedy decoder
-class GreedyCTCDecoder(torch.nn.Module):
-    def __init__(self, labels, blank=0):
-        super().__init__()
-        self.labels = labels
-        self.blank = blank
 
-    def forward(self, emission: torch.Tensor):
+class Decoder(ABC):
+    @abstractmethod
+    def forward(self, emissions: torch.Tensor):
         """Given a sequence emission over labels, get the best path
         Args:
-          emission (Tensor): Logit tensors. Shape `[num_seq, num_label]`.
+            emissions (list, Tensor): Logit tensors. Shape `[batch_size, num_seq, num_label]`.
 
         Returns:
-          List[str]: The resulting transcript
+            List[str]: The resulting transcript
         """
+
+class GreedyCTCDecoder(torch.nn.Module, Decoder):
+    def __init__(self, labels=None, blank=0):
+        super().__init__()
+        if not labels:
+            # hardcoded vocabulary if not provided externally
+            self.labels = ('-','|','E','T','A','O','N','I','H','S','R','D','L','U','M','W','C','F','G','Y','P','B','V','K',"'",'X','J','Q','Z')
+        else:
+            self.labels = labels
+        self.blank = blank
+
+    def forward(self, emissions: torch.Tensor):
+        emission = emissions[0]
         indices = torch.argmax(emission, dim=-1)  # [num_seq,]
         indices = torch.unique_consecutive(indices, dim=-1)
         indices = [i for i in indices if i != self.blank]
         joined = "".join([self.labels[i] for i in indices])
+
         return joined.replace("|", " ").strip().split()
+
+
+class BeamSearchDecoder(Decoder):
+    def __init__(self, lm_files=torchaudio.models.decoder.download_pretrained_files("librispeech-4-gram")):
+        super().__init__()
+        self.beam_search_decoder = torchaudio.models.decoder.ctc_decoder(
+            lexicon=lm_files.lexicon, # giant file of English "words"
+            tokens=lm_files.tokens, # same as wav2vec2's vocab list
+            lm=lm_files.lm, # path to language model binary
+            nbest=3,
+            beam_size=1500,
+            lm_weight=3.23,
+            word_score=-0.26,
+        )
+
+    def forward(self, emissions: torch.Tensor):
+        emission = emissions.cpu().detach()
+        
+        return self.beam_search_decoder(emission)[0][0].words
--- Tools/utils.py
+++ Tools/utils.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import logging
+from bisect import bisect_left
 
-def setup_logging(arg_folder):
+
+def setup_logging_console_folder(arg_folder):
     """Set up logging to both console and a logfile."""
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)  # track INFO logging events (default was WARNING)
@@ -11,3 +13,22 @@ def setup_logging(arg_folder):
     root_logger.addHandler(logging.FileHandler(os.path.join(arg_folder, 'wav2vec2_alignments_run.log'), 'w+'))  # handler to log to file also
     root_logger.handlers[0].setFormatter(logging.Formatter('%(levelname)s:%(asctime)s: %(message)s'))  # log level and message
     root_logger.handlers[1].setFormatter(logging.Formatter('%(levelname)s:%(asctime)s: %(message)s'))
+ 
+
+def BinarySearch(L, x) -> bool:
+    """Find leftmost occurrence of value x in sorted list L using binary search and return True if found"""
+    first = 0
+    last = len(L)-1
+    found = False
+
+    while first<=last and not found:
+         midpoint = (first + last)//2
+         if L[midpoint] == x:
+             found = True
+         else:
+             if x < L[midpoint]:
+                 last = midpoint-1
+             else:
+                 first = midpoint+1
+
+    return found
